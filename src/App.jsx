d6cdaf2af0004c551b10d1434ecad413bd74ca9c
João Eduardo Rocha Alves <joao.eduardo0418@hotmail.com>
2025-04-10 11:50:37 -0300

    feito o featured
    completei a parte featured

--- src/App.jsx
+++ src/App.jsx
@@ -78,23 +78,45 @@ function App() {
         <section className='featured container section'>
           <h3 className='featured__subtitle'>feito por mim</h3>
           <div className='featured__wrapper'>
-          <h2 className='featured__title'>Projeto Google glass</h2>
-          <a className='featured__link' href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-            <path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clip-rule="evenodd" />
-          </svg>
-          </a>
+            <h2 className='featured__title'>Projeto Google glass</h2>
+            <a className='featured__link' href=""><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fill-rule="evenodd" d="M19.902 4.098a3.75 3.75 0 0 0-5.304 0l-4.5 4.5a3.75 3.75 0 0 0 1.035 6.037.75.75 0 0 1-.646 1.353 5.25 5.25 0 0 1-1.449-8.45l4.5-4.5a5.25 5.25 0 1 1 7.424 7.424l-1.757 1.757a.75.75 0 1 1-1.06-1.06l1.757-1.757a3.75 3.75 0 0 0 0-5.304Zm-7.389 4.267a.75.75 0 0 1 1-.353 5.25 5.25 0 0 1 1.449 8.45l-4.5 4.5a5.25 5.25 0 1 1-7.424-7.424l1.757-1.757a.75.75 0 1 1 1.06 1.06l-1.757 1.757a3.75 3.75 0 1 0 5.304 5.304l4.5-4.5a3.75 3.75 0 0 0-1.035-6.037.75.75 0 0 1-.354-1Z" clip-rule="evenodd" />
+            </svg>
+            </a>
           </div>
           <p className='featured__description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias reiciendis totam non quidem quia quibusdam laborum excepturi perferendis repellendus obcaecati consectetur placeat assumenda, asperiores quasi qui nostrum nemo commodi quisquam.</p>
-          <div>
-            <h3 className='featured__label'>TECNOLOGIAS</h3>
-            <ul className='featured__tech-stack'>
-              <li className='featured__info'>html</li>
-              <li className='featured__info'>css</li>
-              <li className='featured__info'>JavaScript</li>
-            </ul>
+          <div className='featured__info-container'>
+            <div>
+              <h3 className='featured__label'>TECNOLOGIAS</h3>
+              <ul className='featured__tech-stack'>
+                <li className='featured__info'>html</li>
+                <li className='featured__info'>css</li>
+                <li className='featured__info'>JavaScript</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className='featured__label'>Tipo de projeto</h3>
+              <p className='featured__info'> Google Glass</p>
+            </div>
+            <div>
+              <h3 className='featured__label'>Linha do tempo</h3>
+              <p className='featured__info'>dezembmro 2023 - presente</p>
+            </div>
           </div>
-          <div></div>
-          <div></div>
+          <div className='featured__img-container'>
+          <div className='featured__img-wrapper'>
+            <img src="/featured1.jpg" alt="imagem do projeto" />
+          </div>
+          <div className='featured__img-wrapper'>
+            <img src="/featured2.jpg" alt="imagem do projeto" />
+          </div>
+          <div className='featured__img-wrapper'>
+            <img src="/featured3.jpg" alt="imagem do projeto" />
+          </div>
+          <div className='featured__img-wrapper'>
+            <img src="/featured4.jpg" alt="imagem do projeto" />
+          </div>
+          </div>
         </section>
         {/* <section></section> */}
         {/* <section></section> */}
