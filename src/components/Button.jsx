82547bff64ac55f8bc87b22d844df94c60ad21e2
João Eduardo Rocha Alves <joao.eduardo0418@hotmail.com>
2025-05-05 15:07:33 -0300

    pequena alteraçao

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import styles from '../styles/button.module.css'
 
 function Button({ text, onClick, className, href }) {
-    // Se houver href, renderiza como link
+    
     if (href) {
         return (
             <a
@@ -15,7 +15,7 @@ function Button({ text, onClick, className, href }) {
         )
     }
 
-    // Senão, renderiza como botão comum
+    
     return (
         <button onClick={onClick} className={`${styles.button} ${className}`}>
             {text}
